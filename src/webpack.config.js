38b935fbb0ab01a3359aac8ecb4544f7193411a2
natasha-t <nthapli@gmail.com>
2017-05-17 22:48:14 -0700

    configure webpack

---
+++ src/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+
+  entry: [
+    './index.js'
+  ],
+
+  output: {
+    filename: 'popup.js',
+    path: path.join(__dirname, '../public'),
+    publicPath: '/'
+  },
+
+  resolve: {
+    extensions: ['', '.js', '.jsx', '.scss', '.json'],
+    modulesDirectories: ['node_modules']  
+  },
+
+
+  module: {
+    loaders: [
+      {
+        test: /\.(jsx|js)?$/,
+        loader: 'babel',
+        exclude: /(node_modules)/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
+
+}
